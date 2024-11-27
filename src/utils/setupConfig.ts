dd6f5b491c9cd3c46cb2368d20a8e1c1b255891a
Rasmus Haversen <rhaversen@gmail.com>
2024-11-27 05:25:46 +0100

    add config

---
+++ src/utils/setupConfig.ts
@@ -0,0 +1,26 @@
+// Node.js built-in modules
+
+// Third-party libraries
+import config from 'config'
+import { type CorsOptions } from 'cors'
+
+// Own modules
+import logger from './logger.js'
+
+// Environment variables
+
+// Config variables
+
+// Destructuring and global variables
+const configString = JSON.stringify(config.util.toObject(config), null, 4)
+
+// Log the configs used
+logger.info(`Using configs:\n${configString}`)
+
+const AppConfig = {
+	expressPort: config.get('expressPort') as number,
+	corsConfig: config.get('cors') as CorsOptions,
+	mainServiceHost: config.get('microservices.codeRunner.host') as string,
+}
+
+export default AppConfig
