6e2b9b16a95e84c25e64d310255fc7c90419ffaf
Rasmus Haversen <rhaversen@gmail.com>
2024-11-27 05:36:16 +0100

    Add services

---
+++ src/services/MainService.ts
@@ -0,0 +1,63 @@
+// Node.js built-in modules
+
+// Third-party libraries
+import axios from 'axios'
+
+// Own modules
+import logger from '../utils/logger.js'
+import AppConfig from '../utils/setupConfig.js'
+
+// Environment variables
+const { MICROSERVICE_AUTHORIZATION } = process.env as Record<string, string>
+
+// Config variables
+const {
+	mainServiceHost
+} = AppConfig
+
+// Destructuring and global variables
+
+export async function createGradingForSubmission(score: number, submissionId: string): Promise<Object | undefined> {
+	try {
+		const response = await axios.post(`http://${mainServiceHost}/api/v1/microservices/grading`, {
+			score,
+			submission: submissionId,
+		}, {
+			headers: {
+				authorization: MICROSERVICE_AUTHORIZATION
+			}
+		})
+
+		logger.info('Grading submitted for submission', submissionId)
+
+		return response.data
+	} catch (error) {
+		if (error instanceof Error) {
+			logger.error('Error submitting grading', { error: error.message })
+		} else {
+			logger.error('Error submitting grading', { error: String(error) })
+		}
+
+		return undefined
+	}
+}
+
+export async function getSubmissions(): Promise<Array<Object> | undefined> {
+	try {
+		const response = await axios.get(`http://${mainServiceHost}/api/v1/microservices/submissions`, {
+			headers: {
+				authorization: MICROSERVICE_AUTHORIZATION
+			},
+		})
+
+		return response.data
+	} catch (error) {
+		if (error instanceof Error) {
+			logger.error('Error getting submissions', { error: error.message })
+		} else {
+			logger.error('Error getting submissions', { error: String(error) })
+		}
+
+		return undefined
+	}
+}
