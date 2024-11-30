5668e9ccee00abf8ef4a1e7b77f895191be0b83f
Rasmus Haversen <rhaversen@gmail.com>
2024-11-30 21:41:21 +0100

    Update controller

--- src/controllers/submissionController.ts
+++ src/controllers/submissionController.ts
@@ -1,8 +1,8 @@
 // Node.js built-in modules
 
 // Third-party libraries
-import { Request, Response } from 'express';
-import { CodeRunnerService } from '../services/CodeRunnerService';
+import { Request, Response } from 'express'
+import { runGame } from '../services/gamerunner/CodeRunnerService'
 
 // Own modules
 
@@ -12,23 +12,19 @@ import { CodeRunnerService } from '../services/CodeRunnerService';
 
 // Destructuring and global variables
 
-const codeRunner = new CodeRunnerService();
-
 export async function gradeSubmission(req: Request, res: Response) {
-    const { code, language } = req.body;
+	const { code } = req.body
 
-    if (!code || !language || typeof code !== 'string' || typeof language !== 'string') {
-        return res.status(400).json({ error: 'Invalid input parameters' });
-    }
+	if (!code || typeof code !== 'string') {
+		return res.status(400).json({ error: 'Invalid input parameters' })
+	}
 
-    if (!codeRunner.isLanguageSupported(language)) {
-        return res.status(400).json({ error: 'Unsupported language' });
-    }
+	const strategies = ['player1', 'player2'] //TODO: Get strategies from database
 
-    try {
-        const result = await codeRunner.executeCode(language, code);
-        res.json(result);
-    } catch (error) {
-        res.status(500).json({ error: error instanceof Error ? error.message : 'Execution failed' });
-    }
+	try {
+		const result = await runGame(code, strategies)
+		res.json(result)
+	} catch (error) {
+		res.status(500).json({ error: error instanceof Error ? error.message : 'Execution failed' })
+	}
 }
