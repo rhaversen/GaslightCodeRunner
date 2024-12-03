8f3170a0b398b2c631a4e852342c2fcf4b648231
Rasmus Haversen <rhaversen@gmail.com>
2024-12-03 17:03:39 +0100

    add test fixture

---
+++ src/development/index.ts
@@ -0,0 +1,22 @@
+/* eslint-disable local/enforce-comment-order */
+// file deepcode ignore NoHardcodedPasswords/test: Hardcoded credentials are only used for testing purposes
+// file deepcode ignore NoHardcodedCredentials/test: Hardcoded credentials are only used for testing purposes
+// file deepcode ignore HardcodedNonCryptoSecret/test: Hardcoded credentials are only used for testing purposes
+
+// Process environment variables
+process.env.NODE_ENV = 'development'
+process.env.SESSION_SECRET = 'TEST_SESSION_SECRET'
+
+async function startServer (): Promise<void> {
+	try {
+		// Start the application server
+		await import('../app/index.js')
+	} catch (error) {
+		console.error('Failed to start the server:', error)
+	}
+}
+
+// Execute the server startup sequence
+await startServer()
+
+export {}
