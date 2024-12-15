bed3bc0f7787e2c5a2d743e9e2637c02be0f150a
Rasmus Haversen <rhaversen@gmail.com>
2024-12-15 03:59:08 +0100

    add tests

--- src/test/unit/Functions/codeRunnerService.spec.ts
+++ src/test/unit/Functions/codeRunnerService.spec.ts
@@ -1,5 +1,3 @@
-/* eslint-disable local/enforce-comment-order */
-
 // file deepcode ignore NoHardcodedPasswords/test: Hardcoded credentials are only used for testing purposes
 // file deepcode ignore NoHardcodedCredentials/test: Hardcoded credentials are only used for testing purposes
 // file deepcode ignore HardcodedNonCryptoSecret/test: Hardcoded credentials are only used for testing purposes
@@ -11,13 +9,20 @@ import { expect } from 'chai'
 import { describe, it } from 'mocha'
 
 // Own modules
-import { runEvaluation } from '../../../app/services/gamerunner/CodeRunnerService.js'
+import { runEvaluation, runTournament } from '../../../app/services/gamerunner/CodeRunnerService.js'
 import {
 	gameFiles,
 	cheatingStrategyFiles,
 	slowStrategyFiles,
 	dumbStrategyFiles,
 } from '../../../app/utils/sourceFiles.js'
+
+// Environment variables
+
+// Config variables
+
+// Destructuring and global variables
+const twoMinuteTimeout = 1200000
 
 // Setup test environment
 import '../../testSetup.js'
@@ -106,7 +111,7 @@ describe('CodeRunnerService', function () {
 		expect(result.error).to.be.a('string')
 	})
 
-	it('should timeout a strategy that takes too long durin evaluation', async function () {
+	it('should timeout a strategy that takes too long during evaluation', async function () {
 		const result = await runEvaluation(
 			gameFiles,
 			slowStrategyFiles,
@@ -123,3 +128,89 @@ describe('CodeRunnerService', function () {
 	})
 })
 
+describe('TournamentService', function () {
+	this.timeout(twoMinuteTimeout)
+	it('should have an error when running a tournament with no strategies', async function () {
+		const result = await runTournament(
+			gameFiles,
+			[],
+			10
+		)
+
+		expect(result).to.not.be.undefined
+		expect(result).to.not.have.property('results')
+		expect(result).to.not.have.property('disqualified')
+		expect(result).to.have.property('error')
+	})
+
+	it('should run a tournament with 1 strategy', async function () {
+		this.timeout(twoMinuteTimeout)
+		const result = await runTournament(
+			gameFiles,
+			[dumbStrategyFiles],
+			10
+		)
+
+		expect(result).to.not.be.undefined
+		expect(result).to.have.property('results')
+		expect(result).to.not.have.property('disqualified')
+		expect(result).to.not.have.property('error')
+	})
+
+	it('should run a tournament with 2 strategies', async function () {
+		this.timeout(twoMinuteTimeout)
+		const result = await runTournament(
+			gameFiles,
+			[dumbStrategyFiles, dumbStrategyFiles],
+			10
+		)
+
+		expect(result).to.not.be.undefined
+		expect(result).to.have.property('results')
+		expect(result).to.not.have.property('disqualified')
+		expect(result).to.not.have.property('error')
+	})
+
+	it('should disqualify a strategy that throws an error during a tournament', async function () {
+		this.timeout(twoMinuteTimeout)
+		const result = await runTournament(
+			gameFiles,
+			[cheatingStrategyFiles, dumbStrategyFiles],
+			10
+		)
+
+		expect(result).to.not.be.undefined
+		expect(result).to.not.have.property('results')
+		expect(result).to.have.property('disqualified')
+		expect(result).to.have.property('error')
+		expect(result.disqualified).to.have.lengthOf(1)
+		expect(result.disqualified![0]).to.be.a('string')
+	})
+
+	it('should include the message when disqualifying a strategy during a tournament', async function () {
+		this.timeout(twoMinuteTimeout)
+		const result = await runTournament(
+			gameFiles,
+			[cheatingStrategyFiles, dumbStrategyFiles],
+			10
+		)
+
+		expect(result.error).to.be.a('string')
+	})
+
+	it('should timeout a strategy that takes too long during a tournament', async function () {
+		this.timeout(twoMinuteTimeout)
+		const result = await runTournament(
+			gameFiles,
+			[slowStrategyFiles, dumbStrategyFiles],
+			10
+		)
+
+		expect(result).to.not.be.undefined
+		expect(result).to.not.have.property('results')
+		expect(result).to.have.property('disqualified')
+		expect(result).to.have.property('error')
+		expect(result.disqualified).to.have.lengthOf(1)
+		expect(result.error).to.include('timed out')
+	})
+})
