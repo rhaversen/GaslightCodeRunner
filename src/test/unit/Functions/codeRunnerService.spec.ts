5abe18276b33f2d9baee93667093ab9a08b023f8
Rasmus Haversen <rhaversen@gmail.com>
2024-12-17 10:52:48 +0100

    Add tests

--- src/test/unit/Functions/codeRunnerService.spec.ts
+++ src/test/unit/Functions/codeRunnerService.spec.ts
@@ -14,8 +14,12 @@ import {
 	gameFiles,
 	cheatingStrategyFiles,
 	slowStrategyFiles,
+	slowLoadingStrategyFiles,
 	dumbStrategyFiles,
 	errorThrowingStrategyFiles,
+	nonHaltingLoadingStrategyFiles,
+	nonHaltingStrategyFiles,
+	chatGptStrategyFiles,
 } from '../../../app/utils/sourceFiles.js'
 
 // Environment variables
@@ -29,6 +33,7 @@ const twoMinuteTimeout = 1200000
 import '../../testSetup.js'
 
 describe('CodeRunnerService', function () {
+	this.timeout(twoMinuteTimeout)
 	it('should run a an evaluation with 1 candidate and 1 other', async function () {
 		const result = await runEvaluation(
 			gameFiles,
@@ -83,8 +88,66 @@ describe('CodeRunnerService', function () {
 		expect(result).to.not.have.property('results')
 		expect(result).to.have.property('disqualified')
 		expect(result).to.have.property('error')
+		expect(result.error?.toLowerCase()).to.not.include('script')
 		expect(result.disqualified).to.have.lengthOf(1)
 		expect(result.disqualified![0]).to.equal('slow')
+		expect(result.error?.toLowerCase()).to.include('strategy execution timed out')
+	})
+
+	it('should return an error when the candidate never halts during evaluation', async function () {
+		this.timeout(twoMinuteTimeout)
+		const result = await runEvaluation(
+			gameFiles,
+			nonHaltingStrategyFiles,
+			[dumbStrategyFiles],
+			10
+		)
+
+		expect(result).to.not.be.undefined
+		expect(result).to.not.have.property('results')
+		expect(result).to.have.property('disqualified')
+		expect(result).to.have.property('error')
+		expect(result.error?.toLowerCase()).to.include('script')
+		expect(result.disqualified).to.have.lengthOf(1)
+		expect(result.disqualified![0]).to.equal(nonHaltingStrategyFiles.submissionId)
+		expect(result.error?.toLowerCase()).to.include('timed out')
+	})
+
+	it('should return an error when the candidate takes too long to load during evaluation', async function () {
+		this.timeout(twoMinuteTimeout)
+		const result = await runEvaluation(
+			gameFiles,
+			slowLoadingStrategyFiles,
+			[chatGptStrategyFiles, dumbStrategyFiles],
+			10
+		)
+
+		expect(result).to.not.be.undefined
+		expect(result).to.not.have.property('results')
+		expect(result).to.have.property('disqualified')
+		expect(result).to.have.property('error')
+		expect(result.error?.toLowerCase()).to.not.include('script')
+		expect(result.disqualified).to.have.lengthOf(1)
+		expect(result.disqualified![0]).to.equal(slowLoadingStrategyFiles.submissionId)
+		expect(result.error?.toLowerCase()).to.include('strategy execution timed out')
+	})
+
+	it('should return an error when the candidate never halts during loading', async function () {
+		this.timeout(twoMinuteTimeout)
+		const result = await runEvaluation(
+			gameFiles,
+			nonHaltingLoadingStrategyFiles,
+			[chatGptStrategyFiles, dumbStrategyFiles],
+			10
+		)
+
+		expect(result).to.not.be.undefined
+		expect(result).to.not.have.property('results')
+		expect(result).to.have.property('disqualified')
+		expect(result).to.have.property('error')
+		expect(result.error?.toLowerCase()).to.include('script')
+		expect(result.disqualified).to.have.lengthOf(1)
+		expect(result.disqualified![0]).to.equal(nonHaltingLoadingStrategyFiles.submissionId)
 		expect(result.error?.toLowerCase()).to.include('timed out')
 	})
 
